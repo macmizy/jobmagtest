ffb82ed82541a1c98aecc4b2e90e47f2985b36f4
macmizy <macmizy@gmail.com>
2025-01-13 02:54:44 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { JobController } from './module/controller';
 
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,7 +17,7 @@ app.use('/api/v1', JobController);
 const PORT =  3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript + Node.js!');
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
